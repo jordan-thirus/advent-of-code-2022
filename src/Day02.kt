8cfdddaf1253a0d3f5ef5377012f4b683305936a
Jordan Thirus <14128480+jordan-thirus@users.noreply.github.com>
2022-12-03 08:57:01 -0500

    Use map for one of day 2's solutions to try it out

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,35 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        fun scoreGame(game: String): Int {
+
+        val scores = buildMap {
+            put(OpponentPlay.ROCK,
+                mapOf(
+                    Play.ROCK to Play.ROCK.score + Outcome.DRAW.score,
+                    Play.PAPER to Play.PAPER.score + Outcome.WIN.score,
+                    Play.SCISSORS to Play.SCISSORS.score + Outcome.LOSE.score))
+            put(OpponentPlay.PAPER,
+                mapOf(
+                    Play.ROCK to Play.ROCK.score + Outcome.LOSE.score,
+                    Play.PAPER to Play.PAPER.score + Outcome.DRAW.score,
+                    Play.SCISSORS to Play.SCISSORS.score + Outcome.WIN.score))
+            put(OpponentPlay.SCISSORS,
+                mapOf(
+                    Play.ROCK to Play.ROCK.score + Outcome.WIN.score,
+                    Play.PAPER to Play.PAPER.score + Outcome.LOSE.score,
+                    Play.SCISSORS to Play.SCISSORS.score + Outcome.DRAW.score))
+        }
+        fun scoreGame2(game: String): Int{
             val opponentPlay = OpponentPlay.get(game[0])
             val selfPlay = Play.get(game[2])
-            val score = when(opponentPlay) {
-                OpponentPlay.ROCK -> {
-                    when(selfPlay) {
-                        Play.ROCK -> Play.ROCK.score + Outcome.DRAW.score
-                        Play.PAPER -> Play.PAPER.score + Outcome.WIN.score
-                        Play.SCISSORS -> Play.SCISSORS.score + Outcome.LOSE.score
-                    }
-                }
-                OpponentPlay.PAPER -> {
-                    when(selfPlay) {
-                        Play.ROCK -> Play.ROCK.score + Outcome.LOSE.score
-                        Play.PAPER -> Play.PAPER.score + Outcome.DRAW.score
-                        Play.SCISSORS -> Play.SCISSORS.score + Outcome.WIN.score
-                    }
-                }
-                OpponentPlay.SCISSORS -> {
-                    when(selfPlay) {
-                        Play.ROCK -> Play.ROCK.score + Outcome.WIN.score
-                        Play.PAPER -> Play.PAPER.score + Outcome.LOSE.score
-                        Play.SCISSORS -> Play.SCISSORS.score + Outcome.DRAW.score
-                    }
-                }
-            }
-            //println("Game '$game' had score $score")
-            return score
+
+            return scores.getOrDefault(opponentPlay, emptyMap()).getOrDefault(selfPlay, 0)
         }
-        return input.sumOf { game -> scoreGame(game) }
+        return input.sumOf { game -> scoreGame2(game) }
     }
 
     fun part2(input: List<String>): Int {
@@ -76,10 +71,10 @@ fun main() {
     println(part2(input))
 }
 
-enum class Outcome(outcome:Char, val score:Int) {
-    LOSE('X',0),
-    DRAW('Y',3),
-    WIN('Z',6);
+enum class Outcome(val score:Int) {
+    LOSE(0),
+    DRAW(3),
+    WIN(6);
 
     companion object {
         fun get(outcome: Char) = when(outcome){
@@ -91,10 +86,10 @@ enum class Outcome(outcome:Char, val score:Int) {
     }
 }
 
-enum class OpponentPlay(val play: Char){
-    ROCK('A'),
-    PAPER('B'),
-    SCISSORS('C');
+enum class OpponentPlay{
+    ROCK,
+    PAPER,
+    SCISSORS;
 
     companion object {
         fun get(play: Char) = when(play){
@@ -106,10 +101,10 @@ enum class OpponentPlay(val play: Char){
     }
 }
 
-enum class Play(val play: Char, val score: Int){
-    ROCK('X', 1),
-    PAPER('Y', 2),
-    SCISSORS('Z', 3);
+enum class Play(val score: Int){
+    ROCK(1),
+    PAPER(2),
+    SCISSORS(3);
 
     companion object {
         fun get(play: Char) = when(play){
