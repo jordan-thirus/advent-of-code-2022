5cee66da40e2a655943f4d4bed9447435b63674a
Jordan Thirus <14128480+jordan-thirus@users.noreply.github.com>
2022-12-10 09:10:37 -0500

    day 10 refactor - handle processing commands in single function - cleaner calculation of signal strength

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,66 +1,61 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var registerX = 1
-        val cycles = mutableListOf<Int>()
+    fun processCommand(command: String, registerX: Int, processCycle: () -> Unit): Int {
+        return when (command) {
+            "noop" -> {
+                processCycle()
+                registerX
+            }
 
-        fun processCycle() = cycles.add(registerX)
-
-        input.forEach { line ->
-            when(line){
-                "noop" -> {
-                    processCycle()
-                }
-                else -> { //addx
-                    val v = line.split(" ")[1].toInt()
-                    processCycle()
-                    processCycle()
-                    registerX += v
-                }
+            else -> { //addx
+                val v = command.split(" ")[1].toInt()
+                processCycle()
+                processCycle()
+                registerX + v
             }
         }
-        val strength20 = cycles[19] * 20
-        val strength60 = cycles[59] * 60
-        val strength100 = cycles[99] * 100
-        val strength140 = cycles[139] * 140
-        val strength180 = cycles[179] * 180
-        val strength220 = cycles[219] * 220
-        return strength20 + strength60 + strength100 + strength140 + strength180 + strength220
+    }
+
+    fun part1(input: List<String>): Int {
+        var registerX = 1
+        var cycle = 1
+        var signalStrength = 0
+
+        input.forEach {
+            registerX = processCommand(it, registerX) {
+                val calculateStrength = (cycle-20)%40 == 0
+                if(calculateStrength){
+                    signalStrength += registerX * cycle
+                }
+                cycle++
+            }
+        }
+
+        return signalStrength
     }
 
     fun part2(input: List<String>) {
         var registerX = 1
         var position = 0
+        val crtWidth = 40
         val crt = mutableListOf<Char>()
 
-        fun processCycle() {
-            if(position in registerX-1..registerX+1){
-                crt.add('#')
-            }
-            else {
-                crt.add('.')
-            }
-            position++
-            if(position == 40){
-                position = 0
+        input.forEach { line ->
+            registerX = processCommand(line, registerX) {
+                if (position in registerX - 1..registerX + 1) {
+                    crt.add('#')
+                } else {
+                    crt.add('.')
+                }
+                position++
+                if (position == crtWidth) {
+                    position = 0
+                }
             }
         }
 
-        input.forEach { line ->
-            when(line){
-                "noop" -> {
-                    processCycle()
-                }
-                else -> { //addx
-                    val v = line.split(" ")[1].toInt()
-                    processCycle()
-                    processCycle()
-                    registerX += v
-                }
-            }
-        }
-        crt.chunked(40)
+        crt.chunked(crtWidth)
             .map { it.joinToString(separator = "") }
-            .forEach{ println(it) }
+            .forEach { println(it) }
 
     }
 
